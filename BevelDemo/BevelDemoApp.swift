8cd63862e1087cf68dbf204172eb4b6c146eec10
dvhull <56657018+dvhull@users.noreply.github.com>
2025-01-30 20:14:40 -0500

    Initial Commit

---
+++ BevelDemo/BevelDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BevelDemoApp.swift
+//  BevelDemo
+//
+//  Created by David Hull on 2025-01-30.
+//
+
+import SwiftUI
+
+@main
+struct BevelDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
