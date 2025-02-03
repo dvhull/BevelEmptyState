65429e3bbf1bd1a365bda6f672fad7f0039b46eb
dvhull <56657018+dvhull@users.noreply.github.com>
2025-02-03 18:50:09 -0500

    re-factored code

--- BevelDemo/CustomViews/CustomViews.swift
+++ BevelDemo/CustomViews/CustomViews.swift
@@ -61,37 +61,3 @@ struct CustomDividerView: View {
             .padding(.horizontal, 5)
     }
 }
-
-// TODO: Probably a better way to do this.
-struct TextWithWeight: UIViewRepresentable {
-    let text: String
-    let weight: CGFloat
-
-    typealias UIViewType = UITextView
-
-    func makeUIView(context: Context) -> UITextView {
-        let view = UITextView()
-        view.text = text
-        view.font = UIFont.systemFont(
-            ofSize: UIFont.preferredFont(forTextStyle: .title3).pointSize,
-            weight: UIFont.Weight(rawValue: weight) // Convert CGFloat to UIFont.Weight
-        )
-        view.isScrollEnabled = false
-        view.isEditable = false
-        view.isSelectable = false
-        view.backgroundColor = .clear
-        return view
-    }
-
-    func updateUIView(_ uiView: UITextView, context: Context) {
-        uiView.text = text
-        uiView.font = UIFont.systemFont(
-            ofSize: UIFont.preferredFont(forTextStyle: .body).pointSize,
-            weight: UIFont.Weight(rawValue: weight)
-        )
-    }
-
-    @MainActor func sizeThatFits(_ proposal: ProposedViewSize, uiView: UITextView, context: Context) -> CGSize? {
-        return uiView.sizeThatFits(proposal.replacingUnspecifiedDimensions())
-    }
-}
